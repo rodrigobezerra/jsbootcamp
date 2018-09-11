ad9dc246b75a9553cdf0849e07da2b0577bca1ba
Rodrigo Bezerra <rodrigo.pinheirob@gmail.com>
2018-09-10 23:05:13 -0400

    Finalizando Seção 4

--- main403.js
+++ main403.js
@@ -1,35 +1,43 @@
-// only works if you execute inside the body
 
-// starting...
-var n1 = parseFloat(prompt("Type a number:"));
-while (isNaN(n1)) {
-    var n1 = prompt("Invalid number! Please, type again your number:");
-    n1 = parseFloat(n1);
+
+function escrever(msg){
+    var output = document.querySelector("#output");
+    output.innerHTML = msg;
 }
 
-function escrever(){
-    var output = document.querySelector("#output");
-    output.innerHTML = n1;
+function start(operator){
+    // only works if you execute inside the body
+
+    // starting...
+    var n1 = parseFloat(prompt("Type the first number:"));
+    while (isNaN(n1)) {
+        var n1 = prompt("Invalid number! Please, type again your number:");
+        n1 = parseFloat(n1);
+    }
+
+    var n2 = parseFloat(prompt("Type the second number:"));
+    while (isNaN(n2)) {
+        var n2 = prompt("Invalid number! Please, type again your number:");
+        n2 = parseFloat(n2);
+    }
+
+    var msg = calcular(operator, n1, n2);
+
+    escrever(msg);
 }
 
 
 /*
  *
  */
-function calcular(op) {
-    var mensagem = "";
-    var n2 = parseFloat(prompt("Type a number:"));
-    while (isNaN(n2)) {
-        var n2 = prompt("Invalid number! Please, type again your number:");
-        n2 = parseFloat(n2);
-    }
+function calcular(operator, n1, n2) {
 
     // The eval function takes a string and then returns the value 
     // of that string considered as a math operation.
-    mensagem = "<i>" + n1 + " " + op + " " + n2 + " = " + eval(n1 + op + n2) + "</i>"; 
-           
+    mensagem = "<i>" + n1 + " " + operator + " " + n2 + " = " + eval(n1 + operator + n2) + "</i>"; 
+      
     mensagem += "<br><b>THAT'S ALL FOLKS \\o/<b>";
 
-    this.output.innerHTML = mensagem;
+    return mensagem;
 }
 
