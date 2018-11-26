d36978f6f256cc68b3bed56c81b756b0578ae558
Rodrigo Bezerra <rodrigo.pinheirob@gmail.com>
2018-11-26 17:48:43 -0400

    Back to arrays...

--- main601.js
+++ main601.js
@@ -1,44 +1,21 @@
 // only works if you execute inside the body
 
-iniciar();
-
 function iniciar() {
-    // starting...
-    var l = prompt("What's the width of the triangle:");
-    l = parseFloat(l);
 
-    var a = prompt("What's the height of the triangle:");
-    a = parseFloat(a);
-    
-    try {
-        var msg = calcularArea(l,a);
-    } catch (e) {
-        msg = e;
-    }
+    var produtos = new Array('pen', 'pencil', 'scisor',
+        'eraser', 'notebook', 'book');
 
-    escrever(msg);
+        produtos.push('compass', 'paper');
 
+        var msg = "";
+        for(let i = 0; i < produtos.length; i++){
+            msg += "Product " + (i + 1) + ": " + produtos[i] + "<br>";
+        }
+
+        escrever(msg);
 }
 
 function escrever(mensagem) {
     var output = document.querySelector("#output");
     output.innerHTML = mensagem;
-}
-
-function calcularArea(l,a) {
-    
-    
-    if (l === undefined || a === undefined){
-        throw new Error("'calcularArea' needs two numbers to complete the triangle calculus!");
-    }
-
-    if (isNaN(l) || isNaN(a)) {
-        throw new Error("Only numbers please!");
-    }
-
-    var area = l * a;
-    var mensagem = "<i> The area of the triangle is: <b>" + l + " * " + a + " = " + area + "</b></i>";
-    mensagem += "<br><b>THAT'S ALL FOLKS \\o/<b>";
-
-    return mensagem;
 }
